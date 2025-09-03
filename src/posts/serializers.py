0ca5d5e8b618e6fa6da7cb8d433f7bbd09f5c30b
Duanne Moraes <duanne.souza@gmail.com>
2025-09-03 11:40:21 -0400

    feat: Add serializers for Post model with creation and update functionality

---
+++ src/posts/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Posts
+
+
+class PostSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(
+        format="%d/%m/%Y %H:%M:%S",
+        read_only=True,
+        )
+    
+    class Meta:
+        model = Posts
+        fields = [
+            "id",
+            "username",
+            "created_at",
+            "title",
+            "content",
+        ]
+        read_only_fields = ["id"]
+
+
+class PostUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Posts
+        fields = ["title", "content"]
