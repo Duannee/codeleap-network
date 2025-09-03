64d16a5500da6c2e6aa5a90f79b67a836622dddb
Duanne Moraes <duanne.souza@gmail.com>
2025-09-03 12:44:27 -0400

    refactor: Update URL routing to use PostUpdateDeleteView instead of PostDetailView

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import PostListCreateView, PostDetailView
+from .views import PostListCreateView, PostUpdateDeleteView
 
 urlpatterns = [
     path("careers/", PostListCreateView.as_view(), name="post-list-create"),
-    path("careers/<int:pk>/", PostDetailView.as_view(), name="post-update-delete"),
+    path("careers/<int:pk>/", PostUpdateDeleteView.as_view(), name="post-update-delete"),
 ]
