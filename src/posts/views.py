ce2a4b5fc1c5131c039f7560c46336d177077ad5
Duanne Moraes <duanne.souza@gmail.com>
2025-09-03 11:57:12 -0400

    feat: Add schema extension for Post views in API documentation

--- src/posts/views.py
+++ src/posts/views.py
@@ -5,13 +5,15 @@ from rest_framework.generics import (
 )
 from .models import Posts
 from .serializers import PostSerializer, PostUpdateSerializer
+from drf_spectacular.utils import extend_schema
 
 
+@extend_schema(tags=["Posts"])
 class PostListCreateView(ListCreateAPIView):
     queryset = Posts.objects.all()
     serializer_class = PostSerializer
 
-
+@extend_schema(tags=["Posts"])
 class PostDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Posts.objects.all()
     serializer_class = PostUpdateSerializer
