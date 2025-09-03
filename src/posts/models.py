0fc80c9f7a06109f1f02036416f5fd923d2401e2
Duanne Moraes <duanne.souza@gmail.com>
2025-09-03 11:30:31 -0400

    feat: Define Posts model with fields for username, created_at, title, and content

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Posts(models.Model):
+    username = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+    def __str__(self) -> str:
+        return f"Post by {self.username}"
