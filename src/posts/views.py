920770ffbac53432362dadcec42684b528ac36d7
Duanne Moraes <duanne.souza@gmail.com>
2025-09-03 11:41:55 -0400

    feat: Implement views for Post model with list, create, and detail functionality

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    
+)
+from .models import Posts
+from .serializers import PostSerializer, PostUpdateSerializer
 
-# Create your views here.
+
+class PostListCreateView(ListCreateAPIView):
+    queryset = Posts.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Posts.objects.all()
+    serializer_class = PostUpdateSerializer
+
+    http_method_names = ["get", "patch", "delete"]
+
