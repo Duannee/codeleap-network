a87b197adaf375952e7ecf89f7a2abbc296adddc
Duanne Moraes <duanne.souza@gmail.com>
2025-09-03 14:33:15 -0400

    feat: Add build script for automated setup and deployment

---
+++ src/build.sh
@@ -0,0 +1,8 @@
+export $(grep -v '^#' .env | xargs)
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
+python manage.py migrate
